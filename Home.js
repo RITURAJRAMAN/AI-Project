e45f4effef7c3aed57a9266030788ddd1ef6da06
IMDKing <riturajraman12345@gmail.com>
2022-07-05 01:15:29 +0530

    commit

--- Home.js
+++ Home.js
@@ -1,32 +1,28 @@
-const toggleButton = document.getElementsByClassName( 'toggle-button' )[ 0 ]
-const textLinks = document.getElementsByClassName( 'text-links' )[ 0 ]
-const navbar = document.getElementsByClassName( 'navbar' )[ 0 ]
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const textLinks = document.getElementsByClassName('text-links')[0]
+const navbar = document.getElementsByClassName('navbar')[0]
 
-const sideToggle = document.getElementsByClassName( 'sideToggle' )[ 0 ]
-const sidebar = document.getElementsByClassName( 'sidebar' )[ 0 ]
-const content = document.getElementsByClassName( 'content' )[ 0 ]
+const sideToggle = document.getElementsByClassName('sideToggle')[0]
+const sidebar = document.getElementsByClassName('sidebar')[0]
+const content = document.getElementsByClassName('content')[0]
 
-toggleButton.addEventListener( 'click', () =>
-{
-    textLinks.classList.toggle( 'active' )
-    navbar.classList.toggle( 'active' )
+toggleButton.addEventListener('click', () => {
+    textLinks.classList.toggle('active')
+    navbar.classList.toggle('active')
 
-    if ( textLinks.classList.length == 2 )
-    {
-        sidebar.classList.remove( 'active' );
-        content.classList.remove( 'active' );
+    if (textLinks.classList.length == 2) {
+        sidebar.classList.remove('active');
+        content.classList.remove('active');
     }
-} )
+})
 
 
-sideToggle.addEventListener( 'click', () =>
-{
+sideToggle.addEventListener('click', () => {
 
-    console.log( sidebar.classList.length )
-    if ( sidebar.classList.length == 1 )
-    {
-        textLinks.classList.remove( 'active' )
+    console.log(sidebar.classList.length)
+    if (sidebar.classList.length == 1) {
+        textLinks.classList.remove('active')
     }
-    sidebar.classList.toggle( 'active' );
-    content.classList.toggle( 'active' );
-} )
+    sidebar.classList.toggle('active');
+    content.classList.toggle('active');
+})
